b54023bfd7f8a51873a9b8b1ea296611371c7620
izisakuya <hbhaotianqi@163.com>
2021-01-27 20:49:35 +0800

    learn interface and struct

--- learn_go_with_tests/interface/myinterface_test.go
+++ learn_go_with_tests/interface/myinterface_test.go
@@ -11,20 +11,21 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 func TestAria(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got '%.2f' but want '%.2f'", got, want)
+	//表格驱动测试
+	areaTest := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "rect", shape: Rectangle{Width: 10.0, Height: 4.0}, hasArea: 40.0},
+		{name: "sphere", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "tri", shape: Triangle{Base: 10.0, Height: 10.0}, hasArea: 50.0},
+	}
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%v got %.2f want %.2f", tt.shape, got, tt.hasArea)
 		}
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 4.0}
-		checkArea(t, rectangle, 40.0)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 
 }
