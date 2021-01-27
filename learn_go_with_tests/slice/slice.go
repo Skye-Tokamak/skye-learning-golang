a540668c037c9f0bb15dc7fe41ca04d8ed7982d3
izisakuya <hbhaotianqi@163.com>
2021-01-27 15:23:22 +0800

    learn slice

--- learn_go_with_tests/slice/slice.go
+++ learn_go_with_tests/slice/slice.go
@@ -1,10 +1,20 @@
 package slice
 
-func Sum(numa [5]int) int {
+func Sum(numa []int) int {
 	var addall int
 	//range用法
 	for _, num := range numa {
 		addall += num
 	}
 	return addall
+}
+
+//可变参数这么用
+func SumAll(numaa ...[]int) []int {
+	var sums []int
+	for _, array := range numaa {
+		sums = append(sums, Sum(array))
+	}
+
+	return sums
 }
--- learn_go_with_tests/slice/slice_test.go
+++ learn_go_with_tests/slice/slice_test.go
@@ -1,12 +1,26 @@
 package slice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numa := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numa)
-	want := 15
-	if got != want {
-		t.Errorf("want '%d' but got '%d' given %v", want, got, numa)
-	}
+	t.Run("collection of elements", func(t *testing.T) {
+		numa := []int{1, 2, 3, 4, 5}
+		got := Sum(numa)
+		want := 15
+		if got != want {
+			t.Errorf("want '%d' but got '%d' given %v", want, got, numa)
+		}
+	})
+	t.Run("collection of arrays", func(t *testing.T) {
+		numaa := []int{1, 2, 3}
+		got := SumAll([]int{1, 2, 3}, []int{1, 2})
+		want := []int{6, 3}
+		//reflect.DeepEqual判断数组内容是否相等
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want '%v' but got '%v' given %v", want, got, numaa)
+		}
+	})
 }
