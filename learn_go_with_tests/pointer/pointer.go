a1d0c2d68f78ed713991839a8cb8019d666aef63
izisakuya <hbhaotianqi@163.com>
2021-01-28 09:59:27 +0800

    learn pointer

--- learn_go_with_tests/pointer/pointer.go
+++ learn_go_with_tests/pointer/pointer.go
@@ -1,4 +1,34 @@
 package pointer
 
+import (
+	"errors"
+	"fmt"
+)
+
+var InsufficientFundsError = errors.New("money not enough")
+
+type Bitcoin int
 type Wallet struct {
+	balance Bitcoin
+}
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+func (w *Wallet) Deposit(income Bitcoin) {
+	w.balance += income
+}
+func (w *Wallet) Withdraw(takeout Bitcoin) error {
+	if w.balance-takeout > 0 {
+		w.balance -= takeout
+	} else {
+		return InsufficientFundsError
+	}
+	return nil
 }
--- learn_go_with_tests/pointer/pointer_test.go
+++ learn_go_with_tests/pointer/pointer_test.go
@@ -3,12 +3,39 @@ package pointer
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	balance := 10
-	if got != balance {
-		t.Errorf("got %d balance %d", got, balance)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s balance %s", got, want)
+		}
 	}
+	assertError := func(t *testing.T, err error, want error) {
+		if err == nil {
+			t.Fatal("want an error but didn't get one")
+		}
+		if err != want {
+			t.Errorf("got err '%s', want '%s'", err, want)
+		}
+	}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("Withdraw too much money", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, Bitcoin(20))
+		assertError(t, err, InsufficientFundsError)
+	})
+
 }
